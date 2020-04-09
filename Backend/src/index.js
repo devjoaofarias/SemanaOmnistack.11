e01584db7a5ec6c057b6ac40e3b5a6663f15d25c
Joao Santos <joao.santos.farias.2003@gmail.com>
2020-04-09 00:34:13 -0300

    Backend conectado com o Frontend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cours = require('cors');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,27 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-/**
- * Rota / Recurso
- */
-
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar/Listar uma informação do backend
- * POST: Adicionar uma informação no backend
- * PUT: Alterar uma informação no backend
- * DELETE: Deletar uma informação do backend
- */
-
- /**
-  * Tipos de parametros:
-  * 
-  * Query: Parametros nomeados na rota após "?"
-  * Route params: Parametros utilizados para identificar recursos
-  * Request body: Corpo da requisição utilizado para criar ou alterar recursos 
-  */
-
-
-
-app.listen(3333);  
+app.listen(3333);
