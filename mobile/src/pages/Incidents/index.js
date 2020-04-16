5e0e4296507c0c9c000270e2ae49d980634a9b9f
Joao Santos <joao.santos.farias.2003@gmail.com>
2020-04-16 01:45:14 -0300

    Falta conectar API

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function Incidents() {
+    const [incidents, setIncidents] = useState([]);
+    const navigation = useNavigation();
+
+    function navigateToDetail() {
+        navigation.navigate('Detail');
+    }
+
+    async function loadIncidents() {
+        const response = await api.get('incidents');
+
+        setIncidents(Response.data)
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image source={logoImg} />
+                <Text style={styles.headerText}>
+                    Total de <Text style={styles.headerTextBold}> 0 casos</Text>.
+            </Text>
+            </View>
+
+            <Text style={styles.title}>Bem Vindo!</Text>
+            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+            <FlatList
+                data={incidents}
+                style={styles.IncidentList}
+                keyExtractor={incident => String(incident.id)}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item: incident }) => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
+
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
+
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>{Intl.NumberFormat
+                        ('pt-BR', {style: 'currency', currency: 'BRL'})
+                        .format(incident.value)}</Text>
+                        <TouchableOpacity
+                            style={styles.detailsButton}
+                            onPress={navigateToDetail}>
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#e02041"></Feather>
+                        </TouchableOpacity>
+                    </View>
+                )} />
+        </View>
+    );
+}
